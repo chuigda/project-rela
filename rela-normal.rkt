007470108aeddfa255a556366c79cbc76112391d
ICEY <icey@icey.tech>
2020-01-08 15:21:45 +0800

    oop frame

--- rela-normal.rkt
+++ rela-normal.rkt
@@ -13,7 +13,7 @@
 
 (struct rl-select-iter (base condition cur-tuples))
 
-(struct rl-equiv-join-iter (iter1 table2 attr1 attr2))
+(struct rl-equiv-join-iter (iter1 table2 attr1-selector attr2-selector))
 
 (struct rl-cproduct-iter (base1 base2 base1-tuples base2-tuples))
 
@@ -24,21 +24,34 @@
 (struct rl-phantom-tuple ())
 
 (define (rl-build-basic-iter table)
-  (rl-basic-iter table (rl-phantom-tuple)))
+  (define (rl-basic-iter-get basic-iter)
+    (let ([current-tuple (rl-basic-iter-cur-tuples basic-iter)])
+      (cond [(rl-phantom-tuple? current-tuple) (error "iterator not ready")]
+            [(null? current-tuple) (error "iterator at end")]
+            [else current-tuple])))
+  (define (rl-basic-iter-next basic-iter)
+    (let ([iter-table (rl-basic-iter-table basic-iter)]
+          [current-tuples (rl-basic-iter-cur-tuples basic-iter)])
+      (rl-basic-iter iter-table
+                     (cond [(rl-phantom-tuple? current-tuples) (rl-table-tuples iter-table)]
+                           [(null? current-tuples) (error "iterator at end")]
+                           [else (cdr current-tuples)]))))
+  (define (rl-basic-iter-at-end? basic-iter)
+    (null (cdr (rl-basic-iter-cur-tuples basic-iter))))
+  (rl-iter (rl-basic-iter table (rl-phantom-tuple))
+           rl-basic-iter-get
+           rl-basic-iter-next
+           rl-basic-iter-at-end?))
 
-(define (rl-basic-iter-get basic-iter)
-  (let ([current-tuple (rl-basic-iter-cur-tuples basic-iter)])
-    (cond [(rl-phantom-tuple? current-tuple) (error "iterator not ready")]
-          [(null? current-tuple) (error "iterator at end")]
-          [else current-tuple])))
 
-(define (rl-basic-iter-next basic-iter)
-  (let ([iter-table (rl-basic-iter-table basic-iter)]
-        [current-tuples (rl-basic-iter-cur-tuples basic-iter)])
-    (rl-basic-iter iter-table
-                   (cond [(rl-phantom-tuple? current-tuples) (rl-table-tuples iter-table))]
-                         [(null? current-tuples) (error "iterator at end")]
-                         [else (cdr current-tuples)])))
-
-(define (rl-basic-iter-at-end? basic-iter)
-  (null (cdr (rl-basic-iter-cur-tuples basic-iter))))
+(define (rl-build-select-iter base condition)
+  (let ([base-get (rl-iter-get base)]
+        [base-next (rl-iter-next base)]
+        [base-test (rl-iter-test base)])
+    (define (rl-select-iter-get select-iter) (error "unimplemented"))
+    (define (rl-select-iter-next select-iter) (error "unimplemented"))
+    (define (rl-select-iter-at-end? select-iter) (error "unimplemented"))
+    (rl-iter (rl-select-iter base condition (rl-phantom-tuple))
+             rl-select-iter-get
+             rl-select-iter-next
+             rl-select-iter-at-end?)))
