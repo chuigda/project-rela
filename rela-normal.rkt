0bb94940c4c8518e265b7946e9b2e105c5b30951
ICEY <icey@icey.tech>
2020-01-08 14:58:36 +0800

    start creating a "normal order" version of rela.rkt

---
+++ rela-normal.rkt
@@ -0,0 +1,40 @@
+#lang racket
+
+(require racket/format)
+
+(define (map-recur proc x)
+  (define (map-one item)
+    (if (list? item) (map map-one item) (proc item)))
+  (map map-one x))
+
+(struct rl-table (name columns tuples))
+
+(struct rl-basic-iter (table cur-tuples))
+
+(struct rl-select-iter (base condition cur-tuples))
+
+(struct rl-cproduct-iter (base1 base2 base1-tuples base2-tuples))
+
+(struct rl-pi-iter (base column-selectors cur-tuples))
+
+(struct rl-phantom-tuple ())
+
+(define (rl-build-iter table)
+  (rl-basic-iter table (rl-phantom-tuple)))
+
+(define (rl-basic-iter-get basic-iter)
+  (let ([current-tuple (rl-basic-iter-cur-tuples basic-iter)])
+    (cond [(rl-phantom-tuple? current-tuple) (error "iterator not ready")]
+          [(null? current-tuple) (error "iterator at end")]
+          [else current-tuple])))
+
+(define (rl-basic-iter-next basic-iter)
+  (let ([iter-table (rl-basic-iter-table basic-iter)]
+        [current-tuples (rl-basic-iter-cur-tuples basic-iter)])
+    (rl-basic-iter iter-table
+                   (cond [(rl-phantom-tuple? current-tuples) (rl-table-tuples iter-table))]
+                         [(null? current-tuples) (error "iterator at end")]
+                         [else (cdr current-tuples)])))
+
+(define (rl-basic-iter-at-end? basic-iter)
+  (null (cdr (rl-basic-iter-cur-tuples basic-iter))))
