1563764bf439aaaba5a6f40a9f99d6eafd749f7c
Alex <alex@ckx-ily.cn>
2020-01-13 14:43:29 +0800

    big update

--- rela-normal.rkt
+++ rela-normal.rkt
@@ -2,82 +2,130 @@
 
 (require racket/format)
 
+; we need this for tree structure traversing
 (define (map-recur proc x)
   (define (map-one item)
     (if (list? item) (map map-one item) (proc item)))
   (map map-one x))
 
+; basic table
 (struct rl-table (name columns tuples))
+
+; interface of dynamic method dispatch
+(struct rl-iter-procs (get next test rewind name columns))
+
+(struct rl-iter (repr procset))
+
+; generic methods
+(define (rl-iter-get iter)
+  (let ([repr (rl-iter-repr iter)]
+        [get-proc (rl-iter-procs-get (rl-iter-procset iter))])
+    (get-proc repr)))
+
+(define (rl-iter-next iter)
+  (let* ([repr (rl-iter-repr iter)]
+         [procs (rl-iter-procset iter)]
+         [next-proc (rl-iter-procs-next procs)])
+    (rl-iter (next-proc repr) procs)))
+
+(define (rl-iter-test iter)
+  (let ([repr (rl-iter-repr iter)]
+        [test-proc (rl-iter-procs-test (rl-iter-procset iter))])
+    (test-proc rer)))
+
+(define (rl-iter-rewind iter)
+  (let* ([repr (rl-iter-repr iter)]
+         [procs (rl-iter-procset iter)]
+         [rewind-proc (rl-iter-procs-rewind iter)])
+    (rl-iter (rewind-proc repr) procs)))
+
+(define (rl-iter-name iter)
+  (let ([repr (rl-iter-repr iter)]
+        [name-proc (rl-iter-procs-name (rl-iter-procset iter))])
+    (name-proc repr)))
+
+(define (rl-iter-columns iter)
+  (let ([repr (rl-iter-repr iter)]
+        [columns-proc (rl-iter-procs-columns (rl-iter-procset iter))])
+    (columns-proc repr)))
+
+; phantom tuple, used when implementing basic-iter
+(struct rl-phantom-tuple ())
 
 (struct rl-basic-iter (table cur-tuples))
 
-(struct rl-select-iter (base condition))
-
-(struct rl-equiv-join-iter (iter1 table2 attr1-selector attr2-selector))
-
-(struct rl-cproduct-iter (base1 base2))
-
-(struct rl-pi-iter (base selectors))
-
-(struct rl-iter (repr get next test))
-
-(struct rl-phantom-tuple ())
-
 (define (rl-build-basic-iter table)
   (define (rl-basic-iter-get basic-iter)
-    (let ([current-tuple (rl-basic-iter-cur-tuples basic-iter)])
-      (cond [(rl-phantom-tuple? current-tuple) (error "iterator not ready")]
-            [(null? current-tuple) (error "iterator at end")]
-            [else current-tuple])))
+    (let ([cur-tuples (rl-basic-iter-cur-tuples basic-iter)])
+      (cond [(rl-phantom-tuple? cur-tuples) (error "iterator not ready")]
+            [(null? cur-tuples) (error "iterator at end")]
+            [else (car cur-tuples)])))
   (define (rl-basic-iter-next basic-iter)
-    (let ([iter-table (rl-basic-iter-table basic-iter)]
-          [current-tuples (rl-basic-iter-cur-tuples basic-iter)])
-      (rl-basic-iter iter-table
-                     (cond [(rl-phantom-tuple? current-tuples) (rl-table-tuples iter-table)]
-                           [(null? current-tuples) (error "iterator at end")]
-                           [else (cdr current-tuples)]))))
-  (define (rl-basic-iter-at-end? basic-iter)
-    (null (cdr (rl-basic-iter-cur-tuples basic-iter))))
-  (rl-iter (rl-basic-iter table (rl-phantom-tuple))
-           rl-basic-iter-get
-           rl-basic-iter-next
-           rl-basic-iter-at-end?))
+    (let ([table (rl-basic-iter-table basic-iter)]
+          [cur-tuples (rl-basic-iter-cur-tuples basic-iter)])
+      (if (null? cur-tuples)
+          (error "iterator already at end")
+          (rl-basic-iter table (cdr cur-tuples)))))
+  (define (rl-basic-iter-test basic-iter)
+    (let ([cur-tuples (rl-basic-iter-cur-tuples basic-iter)])
+      (null? cur-tuples)))
+  (define (rl-basic-iter-rewind basic-iter)
+    (let ([table (rl-basic-iter-table basic-iter)])
+      (rl-basic-iter table (rl-phantom-tuple))))
+  (define (rl-basic-iter-name basic-iter)
+    (let ([table (rl-basic-iter-table basic-iter)])
+      (rl-table-name table)))
+  (define (rl-basic-iter-columns basic-iter)
+    (let ([table (rl-basic-iter-table basic-iter)])
+      (rl-table-columns table)))
+  (rl-iter #|repr|#  (rl-basic-iter table (rl-phantom-tuple))
+           #|procs|# (rl-iter-procs rl-basic-iter-get
+                                    rl-basic-iter-next
+                                    rl-basic-iter-test
+                                    rl-basic-iter-rewind
+                                    rl-basic-iter-name
+                                    rl-basic-iter-columns)))
 
-(define (rl-build-select-iter base condition)
-  (let ([base-get (rl-iter-get base)]
-        [base-next (rl-iter-next base)]
-        [base-test (rl-iter-test base)])
-    (define (rl-select-iter-get select-iter) (error "unimplemented"))
-    (define (rl-select-iter-next select-iter) (error "unimplemented"))
-    (define (rl-select-iter-at-end? select-iter) (error "unimplemented"))
-    (rl-iter (rl-select-iter base condition)
-             rl-select-iter-get
-             rl-select-iter-next
-             rl-select-iter-at-end?)))
+(struct rl-cartesian-iter (base1 base2))
 
-(define (rl-build-cproduct-iter base1 base2)
-  (let ([base1-get (rl-iter-get base1)]
-        [base1-next (rl-iter-next base1)]
-        [base1-test (rl-iter-test base1)]
-        [base2-get (rl-iter-get base2)]
-        [base2-next (rl-iter-next base2)]
-        [base2-test (rl-iter-test base2)])
-    (define (rl-cproduct-iter-get cproduct-iter) (error "unimplemented"))
-    (define (rl-cproduct-iter-next cproduct-iter) (error "unimplemented"))
-    (define (rl-cproduct-iter-at-end? cproduct-iter) (error "unimplemented"))
-    (rl-iter (rl-cproduct-iter base1 base2)
-             rl-cproduct-iter-get
-             rl-cproduct-iter-next
-             rl-cproduct-iter-at-end?)))
+(define (rl-build-cartesian-iter base1 base2)
+  (define (rl-cartesian-iter-get cartesian-iter)
+    (append (rl-iter-get base1) (rl-iter-get base2)))
+  (define (rl-cartesian-iter-next cartesian-iter)
+    (let ([base1 (rl-cartesian-iter-base1 cartesian-iter)]
+          [base2 (rl-cartesian-iter-base2 cartesian-iter)])
+      (cond [(rl-iter-test base1) (error "iterator at end")]
+            [(rl-iter-test base2) (rl-cartesian-iter (rl-iter-next base1) 
+                                                     (rl-iter-next (rl-iter-rewind base2)))]
+            [else (rl-cartesian-iter base1 (rl-iter-next base2))])))
+  (define (rl-cartesian-iter-test cartesian-iter)
+    (let ([base1 (rl-cartesian-iter-base1 cartesian-iter)]
+      (rl-iter-test base1))))
+  (define (rl-cartesian-iter-rewind cartesian-iter)
+    (let ([base1 (rl-cartesian-iter-base1 cartesian-iter)]
+          [base2 (rl-cartesian-iter-base2 cartesian-iter)])
+      (rl-cartesian-iter (rl-iter-next (rl-iter-rewind base1))
+                         (rl-iter-rewind base2))))
+  (define (rl-cartesian-iter-name cartesian-iter)
+    (let ([base1 (rl-cartesian-iter-base1 cartesian-iter)]
+          [base2 (rl-cartesian-iter-base2 cartesian-iter)]
+      (string-append "<" (rl-iter-name base1) " * " (rl-iter-name base2) ">"))))
+  (define (rl-cartesian-iter-columns cartesian-iter)
+    (let ([base1 (rl-cartesian-iter-base1 cartesian-iter)]
+          [base2 (rl-cartesian-iter-base2 cartesian-iter)])
+      (append (rl-cartesian-iter-columns base1) (rl-cartesian-iter-columns base2))))
+  (rl-iter #|repr|#  (rl-cartesian-iter base1 (rl-iter-next base2))
+           #|procs|# (rl-iter-procs rl-cartesian-iter-get
+                                    rl-cartesian-iter-next
+                                    rl-cartesian-iter-test
+                                    rl-cartesian-iter-rewind
+                                    rl-cartesian-iter-name
+                                    rl-cartesian-iter-columns))
 
-(define (rl-build-pi-iter base selectors)
-  (let ([base-get (rl-iter-get base)]
-        [base-next (rl-iter-next base)]
-        [base-test (rl-iter-test base)])
-    (define (rl-pi-iter-get pi-iter) (error "unimplemented"))
-    (define (rl-pi-iter-next pi-iter) (error "unimplemented"))
-    (define (rl-pi-iter-at-end? pi-iter) (error "unimplemented"))
-    (rl-iter (rl-pi-iter base selectors)
-             rl-pi-iter-get
-             rl-pi-iter-next
-             rl-pi-iter-at-end?)))
+(struct rl-pi-iter (base))
+
+(struct rl-select-iter (base))
+
+(struct rl-indexed-select-iter (base))
+
+(struct rl-equiv-join-iter (iter1 table2))
